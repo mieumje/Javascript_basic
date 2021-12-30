034dff608bcdb0964422897ed663c34a64896b24
mieumje <66072832+mieumje@users.noreply.github.com>
2021-12-30 23:07:57 +0900

    Feat : Getting Username
    input값을 username으로 가져옴
    
    loginForm.classList.add(HIDDEN_CLASSNAME)
    greeting.classList.remove(HIDDEN_CLASSNAME)
    
    class를 추가하고 지우는것을 통해 로그인 되었을 때 화면 표시 변경

--- momentum/src/app.js
+++ momentum/src/app.js
@@ -1,2 +1,13 @@
-console.log('Hello');
-alert('hi');
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden"; // String으로만 이루어진 경우 Uppercase로 변수명 지정
+const onLoginSubmit = (e) => {
+    e.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit)
