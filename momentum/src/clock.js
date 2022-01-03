340052665c3eeb714a488f6a3735f6c4b7d6c6c3
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-03 23:45:44 +0900

    Refactor : 현재 시각 표시
    1시, 1분, 1초의 경우 1:1:1로 표시되는 것을
    01:01:01로 표시되도록 변경

--- momentum/src/clock.js
+++ momentum/src/clock.js
@@ -2,5 +2,9 @@ import { clock } from './htmlDom.js';
 
 export const getDate = () => {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const Hours = (`0${date.getHours()}`).slice(-2);
+  const Minutes = (`0${date.getMinutes()}`).slice(-2);
+  const Seconds = (`0${date.getSeconds()}`).slice(-2);
+
+  clock.innerText = `${Hours}:${Minutes}:${Seconds}`;
 };
