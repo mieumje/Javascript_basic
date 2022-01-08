8d174d4c625314fb982478e0a3f50a68d6a82725
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-08 23:33:19 +0900

    Feat : Background Images
    배경 사진 입력 중 에러 발생
    자바스크립트 모듈을 사용하는 상황에서 에러 발생
    
    Failed to load module script:
    Expected a JavaScript module script but the server responded with a MIME type of "image/jpeg".
    Strict MIME type checking is enforced for module scripts per HTML spec.

--- momentum/src/app.js
+++ momentum/src/app.js
@@ -1,9 +1,12 @@
 import { getUserName, paintMain } from './user.js';
 import { getDate } from './clock.js';
+import { chosenImage, backgroundImage } from './background.js';
 
 export default function momentum() {
   paintMain(getUserName);
   getDate();
   setInterval(getDate, 1000);
+  document.body.appendChild(backgroundImage);
+  console.log(backgroundImage.src);
 }
 momentum();
