4e6b29dace750de870d447e061a2ee9229333443
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-11 00:28:38 +0900

    Refcator : todo
    paintToDo 함수 작성
    input에서 가져온 todo list를 작성하기 위함
    
    ul tag안에 li와 span을 작성
    
    eslint 적용

--- momentum/src/todo.js
+++ momentum/src/todo.js
@@ -2,10 +2,21 @@ const toDoForm = document.querySelector('#todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.querySelector('#todo-list');
 
+function paintToDo(newToDoValue) {
+  const newLi = document.createElement('li');
+  const newSpan = document.createElement('span');
+
+  newLi.appendChild(newSpan);
+  newSpan.innerText = newToDoValue;
+
+  toDoList.appendChild(newLi);
+}
+
 function onToDoFormSubmtin(e) {
-    e.preventDefault();
-    const newToDoValue = toDoInput.value;
-    toDoInput.value = ""
+  e.preventDefault();
+  const newToDoValue = toDoInput.value;
+  toDoInput.value = '';
+  paintToDo(newToDoValue);
 }
 
 toDoForm.addEventListener('submit', onToDoFormSubmtin);
