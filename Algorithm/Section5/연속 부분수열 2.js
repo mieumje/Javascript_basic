440b0de9e2b6df1da4eecbb658bb6c33042d5b41
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-28 13:26:07 +0900

    Section 5 : 연속 부분수열 2
    해당 idx에서 연속 부분 수열을 구하기
    해당 idx - 부분 수열의 시작 idx + 1

--- Algorithm/Section5/연속 부분수열 2.js
+++ Algorithm/Section5/연속 부분수열 2.js
@@ -1,9 +1,18 @@
+console.time('label');
 function solution(m, arr) {
     let answer = 0;
     let length = arr.length;
-
+    let sum = left = 0;
+    for (let right = 0; right < length; right++) {
+        sum += arr[right];
+        while (sum > m) {
+            sum -= arr[left++];
+        }
+        answer += (right - left) + 1;
+    }
     return answer;
 }
-
+console.timeEnd('label');
 let a = [1, 3, 1, 2, 3];
 console.log(solution(5, a));
+// solution  time = 0.07ms
