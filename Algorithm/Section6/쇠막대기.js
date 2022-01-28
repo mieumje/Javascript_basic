448429fcff811c61a088dd5623f75c90ccf32fb3
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-28 23:56:48 +0900

    Section 6 : 쇠막대기

--- Algorithm/Section6/쇠막대기.js
+++ Algorithm/Section6/쇠막대기.js
@@ -1,10 +1,18 @@
 function solution(s) {
     let answer = 0;
-    for (let x of s) {
-        console.log(x);
+    let tmp = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === ')' && s[i - 1] === '(') {
+            tmp.pop();
+            answer += tmp.length;
+        } else if (s[i] === ')' && s[i - 1] !== '(') {
+            tmp.pop();
+            answer += 1;
+        } else tmp.push(s[i]);
     }
     return answer;
 }
 
 let a = "()(((()())(())()))(())";
-console.log(solution(a));
+let b = "(((()(()()))(())()))(()())";
+console.log(solution(a), solution(b));
