6fa6a583c04eec042b2e7ae7cbbba3874870c025
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-31 13:25:51 +0900

    Section 7
    장난꾸러기 현수

--- Algorithm/Section7/장난꾸러기 현수.js
+++ Algorithm/Section7/장난꾸러기 현수.js
@@ -1,8 +1,27 @@
 function solution(arr) {
     let answer = [];
-
+    let h = j = 0;
+    let last = arr[arr.length - 1];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] > last) {
+            h = i + 1;
+        } else {
+            last = arr[i];
+        }
+    }
+    let first = arr[0];
+    for (let i = 1; i < arr.length - 1; i++) {
+        if (first > arr[i]) {
+            j = i + 1;
+        } else {
+            first = arr[i];
+        }
+    }
+    console.log(h, j)
     return answer;
 }
 
 let arr = [120, 125, 152, 130, 135, 135, 143, 127, 160];
+let brr = [120, 130, 150, 150, 130, 150];
 console.log(solution(arr));
+console.log(solution(brr));
