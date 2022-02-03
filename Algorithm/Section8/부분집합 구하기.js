2bef0fbda52c1a09ca90750996ba8acc7fcf9940
mieumje <66072832+mieumje@users.noreply.github.com>
2022-02-03 16:36:08 +0900

    Section 8
    부분집합 구하기

---
+++ Algorithm/Section8/부분집합 구하기.js
@@ -0,0 +1,22 @@
+function solution(n) {
+    let answer = [];
+    let ch = Array.from({ length: n + 1 }, () => 0);
+    function DFS(v) {
+        if (v === n + 1) {
+            let tmp = '';
+            for (let i = 1; i < ch.length; i++) {
+                if (ch[i] === 1) tmp += i + " ";
+            }
+            if (tmp.length > 0) answer.push(tmp.trim());
+            return
+        }
+        ch[v] = 1;
+        DFS(v + 1);
+        ch[v] = 0;
+        DFS(v + 1);
+    }
+    DFS(1);
+    return answer;
+}
+
+console.log(solution(3));
