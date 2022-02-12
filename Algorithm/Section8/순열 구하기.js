5960fd18698a51928b6784f6cc74a6ce72c4ca1a
mieumje <66072832+mieumje@users.noreply.github.com>
2022-02-12 13:04:53 +0900

    Section 8
    순열 구하기
    팩토리얼 -> (n부터 1까지 차감)

--- Algorithm/Section8/순열 구하기.js
+++ Algorithm/Section8/순열 구하기.js
@@ -1,12 +1,11 @@
 function solution(m, arr) {
-    let answer = [];
-    n = arr.length;
+    let result = [];
+    let n = arr.length;
     let tmp = Array.from({ length: m }, () => 0);
     let check = Array.from({ length: n }, () => 0);
+
     function DFS(L) {
-        if (L === m) {
-            answer.push(tmp.slice());
-        }
+        if (L === m) result.push(tmp.slice());
         else {
             for (let i = 0; i < n; i++) {
                 if (check[i] === 0) {
@@ -15,12 +14,14 @@ function solution(m, arr) {
                     DFS(L + 1);
                     check[i] = 0;
                 }
+                console.log(tmp, check)
             }
         }
     }
     DFS(0);
-    let result = [answer, answer.length];
-    return result;
+
+    let answer = [result, result.length];
+    return answer;
 }
 
 let arr = [3, 6, 9];
--- Algorithm/Section8/팩토리얼.js
+++ Algorithm/Section8/팩토리얼.js
@@ -10,5 +10,15 @@ function solution(n) {
     DFS(1);
     return answer;
 }
+function solution2(n) {
+    let answer;
+    function DFS(L) {
+        if (L === 1) return 1;
+        else return L * DFS(L - 1);
+    }
+    answer = DFS(n);
+    return answer
+}
 
 console.log(solution(5));
+console.log(solution2(5));
