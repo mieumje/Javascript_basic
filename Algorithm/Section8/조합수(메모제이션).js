3fd670d8293e4e4dddb95fac8aea22a55c8de5a7
mieumje <66072832+mieumje@users.noreply.github.com>
2022-02-13 15:55:01 +0900

    Section 8
    Refactor 조합수(메모제이션)

--- Algorithm/Section8/조합수(메모제이션).js
+++ Algorithm/Section8/조합수(메모제이션).js
@@ -2,14 +2,15 @@ console.time('label');
 function solution(n, r) {
     let answer = 0;
     function DFS(n, r) {
-        if (n === r) return answer += 1;
-        if (r === 1) return answer += n;
-        else return DFS(n - 1, r - 1), DFS(n - 1, r)
+        if (n === r) return 1
+        else if (r === 1) return n
+        else return DFS(n - 1, r - 1) + DFS(n - 1, r)
     }
-    DFS(n, r);
+    answer = DFS(n, r);
     return answer;
 }
 // console.log(solution(5, 3));
 console.log(solution(33, 19));
 console.timeEnd('label');
-// n = 33, r = 19 > time = 3.668s
+// n = 33, r = 19 > time = 3.668s 첫번째 시도
+// n = 33, r = 19 > time = 3.623s 두번째 시도
